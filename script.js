a2c259f3c3c732195bc5fff847011e23c45ab7f9
Animation Termination <86012532+AnimationT@users.noreply.github.com>
2025-10-05 09:21:48 +0000

    made some more changes

--- script.js
+++ script.js
@@ -3,29 +3,46 @@ var leftValue = 0;
 
 var character2 = $("#character2");
 var leftValue2 = 0;
-
+var countdownTimeLeft = 3;
 setTimeout(function () {
-  $(document).keydown(function (event) {
-    if (event.key === " ") {
+  $(document).keyup(function (event) {
+    if (event.key === " " && leftValue < 950) {
       leftValue += 10;
       character.css({
         left: leftValue + "px",
       });
-    } else {
+    } else if (event.key !== " ") {
       $("#info").text("press space to run");
+      setTimeout(() => {
+        $("#info").text(" ");
+      }, 1000);
+    } else {
+      $("#info").text("you finished!");
     }
   });
 }, 3000);
 
 setTimeout(function () {
   setInterval(() => {
-    leftValue2 += 10;
-    character2.css({
-      left: leftValue2 + "px",
-    });
-  }, 200);
+    if (leftValue2 < 950) {
+      leftValue2 += 10;
+      character2.css({
+        left: leftValue2 + "px",
+      });
+    } else if (leftValue < 950) {
+      $("#botInfo").text("bot finished before you");
+    }
+  }, 140);
 }, 3000);
 
-for (let countdown = 3; step > 0; step--) {
-  $("#info").text(countdown);
-}
+$("#info").text(countdownTimeLeft);
+
+var countdown = setInterval(() => {
+  countdownTimeLeft--;
+  $("#info").text(countdownTimeLeft);
+
+  if (countdownTimeLeft <= 0) {
+    clearInterval(countdown);
+    $("#info").text("GO!");
+  }
+}, 1000);
